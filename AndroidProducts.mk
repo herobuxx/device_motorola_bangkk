97a03d85829b0ccabacdf3163e81be3230ca0fe9
HeroBuxx <me@herobuxx.me>
2026-02-19 19:20:53 +0000

    bangkk: Enchanting Bloom
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,4 +4,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_bangkk.mk
+    $(LOCAL_DIR)/halcyon_bangkk.mk
--- halcyon_bangkk.mk
+++ halcyon_bangkk.mk
@@ -11,10 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from bangkk device
 $(call inherit-product, device/motorola/bangkk/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-PRODUCT_NAME := lineage_bangkk
+PRODUCT_NAME := halcyon_bangkk
 PRODUCT_DEVICE := bangkk
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
